822f63dd5c8811c7bcec63b433b4ae1fd116f54a
whxcer <whxcer@outlook.com>
2024-06-15 22:18:48 +0800

    Redux 和 React 提交 action 传参

--- src/components/TestReduxToolkitAndReactRedux.js
+++ src/components/TestReduxToolkitAndReactRedux.js
@@ -1,6 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux'
 // 导入 actionCreater
-import { increment, decrement } from '../store/modules/counterStore'
+import { increment, decrement, jumpToNum } from '../store/modules/counterStore'
 
 function TestReduxToolkitAndReactRedux() {
     const { count } = useSelector(state => state.counter)
@@ -13,6 +13,8 @@ function TestReduxToolkitAndReactRedux() {
                 <span>count is { count }</span>
                 <button onClick={() => dispatch(increment())}>increment</button>
                 <button onClick={() => dispatch(decrement())}>decrement</button>
+                <button onClick={() => dispatch(jumpToNum(10))}>jump to 10</button>
+                <button onClick={() => dispatch(jumpToNum(20))}>jump to 20</button>
             </div>
         </div>
     )
--- src/store/modules/counterStore.js
+++ src/store/modules/counterStore.js
@@ -8,22 +8,25 @@ const counterStore = createSlice({
     },
     // 修改数据的同步方法
     reducers: {
-        increment (state) {
+        increment(state) {
             state.count++
         },
-        decrement (state) {
+        decrement(state) {
             state.count--
+        },
+        jumpToNum(state, action) {
+            state.count = action.payload
         },
     }
 })
 
 // 解构出创建 action 对象的函数 actionCreater
-const { increment, decrement } = counterStore.actions
+const { increment, decrement, jumpToNum } = counterStore.actions
 // 获取 reducer 函数
 const counterReducer = counterStore.reducer
 
 // 导出创建 action 对象的函数和 reducer 函数
 // 以按需导出的方式导出 actionCreater
-export { increment, decrement }
+export { increment, decrement, jumpToNum }
 // 以默认导出的方式导出 reducer
 export default counterReducer
