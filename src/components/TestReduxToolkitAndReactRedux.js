683587a4ae4d58306def44cbbb28c4693b80f4c9
whxcer <whxcer@outlook.com>
2024-06-16 22:49:34 +0800

    Redux和React：异步状态操作

--- src/components/TestReduxToolkitAndReactRedux.js
+++ src/components/TestReduxToolkitAndReactRedux.js
@@ -1,20 +1,36 @@
 import { useSelector, useDispatch } from 'react-redux'
 // 导入 actionCreater
 import { increment, decrement, jumpToNum } from '../store/modules/counterStore'
+import { fetchChannelList } from '../store/modules/channelStore'
+import { useEffect } from 'react'
 
 function TestReduxToolkitAndReactRedux() {
     const { count } = useSelector(state => state.counter)
     const dispatch = useDispatch()
+    const { channelList } = useSelector(state => state.channel)
+
+    // 使用 useEffect 触发异步请求执行
+    useEffect(() => {
+        dispatch(fetchChannelList())
+    }, [dispatch])
 
     return (
         <div>
             <div>TestReduxToolkitAndReactRedux</div>
             <div>
                 <span>count is { count }</span>
-                <button onClick={() => dispatch(increment())}>increment</button>
                 <button onClick={() => dispatch(decrement())}>decrement</button>
+                <button onClick={() => dispatch(increment())}>increment</button>
                 <button onClick={() => dispatch(jumpToNum(10))}>jump to 10</button>
                 <button onClick={() => dispatch(jumpToNum(20))}>jump to 20</button>
+            </div>
+
+            <div>
+                <ul>
+                    {
+                        channelList.map(item => <li key={item.id}>{item.name}</li>)
+                    }
+                </ul>
             </div>
         </div>
     )
--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit"
 
 // 导出子模块 reducer
 import counterReducer from "./modules/counterStore"
+import channelReducer from "./modules/channelStore"
 
 // 创建根 store 组合子模块
 const store = configureStore({
     reducer: {
-        counter: counterReducer
+        counter: counterReducer,
+        channel: channelReducer,
     }
 })
 
